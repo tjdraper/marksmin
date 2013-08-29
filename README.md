66e7783790e1442c20f3d0448011c71fe6249293
Michael Leigeber <michael@caddis.co>
2013-08-29 09:07:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+ExpressionEngine HTML Minification
+====
+
+This extensions uses the Minify (https://code.google.com/p/minify/) library to minify EE rendered markup.
+
+Usage:
+
+Install and enable the extension. The following two settings are applicable for your config file.
+
+$config['marksmin_enabled'] = true;
+$config['marksmin_xhtml'] = false;
---
+++ system/expressionengine/third_party/marksmin/ext.marksmin.php
@@ -0,0 +1,99 @@
+<?php if (! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * @package  Marksmin
+ * @author   Michael Leigeber
+ * @license  http://www.apache.org/licenses/LICENSE-2.0
+ * @link     http://www.caddis.co
+ */
+
+class Marksmin_ext {
+
+	public $EE;
+	public $name = 'Marksmin';
+	public $version = '1.0.0';
+	public $description = 'Minify HTML output';
+	public $docs_url = '';
+	public $settings_exist = 'n';
+	public $config;
+
+	/**
+	 * Constructor
+	 *
+	 * @param  mixed Settings array or empty string if none exist
+	 * @return void
+	 */
+	public function __construct($settings = array())
+	{
+		$this->EE =& get_instance();
+		$this->settings = $settings;
+	}
+
+	/**
+	 * Activate Extension
+	 * 
+	 * @return void
+	 */
+	public function activate_extension()
+	{
+		$this->EE->db->insert('extensions',
+			array(
+				'class' => __CLASS__,
+				'method' => 'template_post_parse',
+				'hook' => 'template_post_parse',
+				'settings' => '',
+				'priority' => 10,
+				'version' => $this->version,
+				'enabled' => 'y'
+			)
+		);
+	}
+
+	/**
+	 * Disable Extension
+	 *
+	 * @return void
+	 */
+	public function disable_extension()
+	{
+		$this->EE->db->where('class', __CLASS__);
+		$this->EE->db->delete('extensions');
+	}
+
+	/**
+	 * Method for template_post_parse hook
+	 *
+	 * @param  string  Parsed template string
+	 * @param  bool    Whether an embed or not
+	 * @param  integer Site ID
+	 * @return string  Template string
+	 */
+	public function template_post_parse($template, $sub, $site_id)
+	{
+		// Play nice with other extensions
+		if (isset($this->EE->extensions->last_call) && $this->EE->extensions->last_call)
+		{
+			$template = $this->EE->extensions->last_call;
+		}
+
+		// Do nothing if not final template
+		if ($sub !== FALSE)
+		{
+			return $template;
+		}
+
+		// Is HTML minification disabled
+		if ($this->EE->config->item('marksmin_enabled') !== true)
+		{
+			return $template;
+		}
+
+		require_once('libraries/Minify/HTML.php');
+
+		$options = array();
+
+		$options['xhtml'] = $this->EE->config->item('marksmin_xhtml');
+
+		return Minify_HTML::minify($template, $options);
+	}
+}
