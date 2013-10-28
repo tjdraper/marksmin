03d442be21134ce89d90c0a184a19eff678d50b4
Michael Leigeber <michael@leigeber.com>
2013-10-28 11:01:37 -0500

    1.1.1 Push

--- system/expressionengine/third_party/marksmin/ext.marksmin.php
+++ system/expressionengine/third_party/marksmin/ext.marksmin.php
@@ -9,13 +9,11 @@
 
 class Marksmin_ext {
 
-	public $EE;
 	public $name = 'Marksmin';
-	public $version = '1.1.0';
+	public $version = '1.1.1';
 	public $description = 'Minify HTML output';
 	public $docs_url = '';
 	public $settings_exist = 'n';
-	public $config;
 
 	/**
 	 * Constructor
@@ -35,17 +33,33 @@ class Marksmin_ext {
 	 */
 	public function activate_extension()
 	{
-		ee()->db->insert('extensions',
-			array(
-				'class' => __CLASS__,
-				'method' => 'template_post_parse',
-				'hook' => 'template_post_parse',
-				'settings' => '',
-				'priority' => 10,
-				'version' => $this->version,
-				'enabled' => 'y'
-			)
-		);
+		ee()->db->insert('extensions', array(
+			'class' => __CLASS__,
+			'method' => 'template_post_parse',
+			'hook' => 'template_post_parse',
+			'settings' => '',
+			'priority' => 10,
+			'version' => $this->version,
+			'enabled' => 'y'
+		));
+	}
+
+	/**
+	 * Update Extension
+	 *
+	 * @return mixed void on update / false if none
+	 */
+	public function update_extension($current = '')
+	{
+		if ($current == '' or $current == $this->version)
+		{
+			return false;
+		}
+
+		ee()->db->where('class', __CLASS__);
+		ee()->db->update('extensions', array(
+			'version' => $this->version
+		));
 	}
 
 	/**
@@ -69,35 +83,37 @@ class Marksmin_ext {
 	 */
 	public function template_post_parse($template, $sub, $site_id)
 	{
-		if (ee()->TMPL->template_type != 'webpage')
+		$type = ee()->TMPL->template_type;
+
+		if ($type == 'webpage' or $type == 'cp_asset')
 		{
-			return $template;
+			// Play nice with other extensions
+			if (isset(ee()->extensions->last_call) and ee()->extensions->last_call)
+			{
+				$template = ee()->extensions->last_call;
+			}
+
+			// Do nothing if not final template
+			if ($sub !== false)
+			{
+				return $template;
+			}
+
+			// Is HTML minification disabled
+			if (ee()->config->item('marksmin_enabled') !== true)
+			{
+				return $template;
+			}
+
+			require_once('libraries/Minify/HTML.php');
+
+			$options = array(
+				'xhtml' => ee()->config->item('marksmin_xhtml')
+			);
+
+			return Minify_HTML::minify($template, $options);
 		}
 
-		// Play nice with other extensions
-		if (isset(ee()->extensions->last_call) && ee()->extensions->last_call)
-		{
-			$template = ee()->extensions->last_call;
-		}
-
-		// Do nothing if not final template
-		if ($sub !== FALSE)
-		{
-			return $template;
-		}
-
-		// Is HTML minification disabled
-		if (ee()->config->item('marksmin_enabled') !== true)
-		{
-			return $template;
-		}
-
-		require_once('libraries/Minify/HTML.php');
-
-		$options = array();
-
-		$options['xhtml'] = ee()->config->item('marksmin_xhtml');
-
-		return Minify_HTML::minify($template, $options);
+		return $template;
 	}
 }
